e85c00245fbd64168d86625495f755ff7271d9f2
Rodney Coleman <rc6886@gmail.com>
2017-08-03 13:12:04 -0500

    Adding intial setup for React view/spa.

--- Bookshop.WebApi/src/index.js
+++ Bookshop.WebApi/src/index.js
@@ -1,13 +1,43 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-export var Hello = function () {
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+var Navbar = function () {
+    return (React.createElement("nav", { className: "navbar navbar-toggle-md navbar-inverse bg-inverse fixed-top" },
+        React.createElement("a", { className: "navbar-brand", href: "#" }, "Bookshop")));
+};
+var BookCard = function (_a) {
+    var cardTitle = _a.cardTitle, cardText = _a.cardText;
+    return (React.createElement("div", { className: "card", style: { maxWidth: "300px" } },
+        React.createElement("img", { className: "card-img-top", src: "...", alt: "Card image cap", style: { maxWidth: "100%" } }),
+        React.createElement("div", { className: "card-block" },
+            React.createElement("h4", { className: "card-title" }, cardTitle),
+            React.createElement("p", { className: "card-text" }, cardText),
+            React.createElement(Link, { to: "/books", className: "btn btn-primary" }, "Go to Book"))));
+};
+var BookDetail = function () {
+    return (React.createElement("div", null, "This is the book detai page."));
+};
+export var BookshopApp = function () {
     return (React.createElement("div", null,
-        React.createElement("nav", { className: "navbar navbar-toggle-md navbar-inverse bg-inverse fixed-top" },
-            React.createElement("a", { className: "navbar-brand", href: "#" }, "Bookshop")),
+        React.createElement(Navbar, null),
         React.createElement("div", { className: "container" },
             React.createElement("div", { className: "bookshop" },
                 React.createElement("h1", null, "Bookshop"),
-                React.createElement("div", { className: "row" })))));
+                React.createElement("div", { className: "row" },
+                    React.createElement("div", { className: "col-md-4 mt-3" },
+                        React.createElement(BookCard, { cardTitle: "Test", cardText: "Some quick example text to build on the card title and make up the bulk of the card's content." })),
+                    React.createElement("div", { className: "col-md-4 mt-3" },
+                        React.createElement(BookCard, { cardTitle: "Test2", cardText: "Some quick example text to build on the card title and make up the bulk of the card's content." })),
+                    React.createElement("div", { className: "col-md-4 mt-3" },
+                        React.createElement(BookCard, { cardTitle: "Test2", cardText: "Some quick example text to build on the card title and make up the bulk of the card's content." })),
+                    React.createElement("div", { className: "col-md-4 mt-3" },
+                        React.createElement(BookCard, { cardTitle: "Test2", cardText: "Some quick example text to build on the card title and make up the bulk of the card's content." })))))));
 };
-ReactDOM.render(React.createElement(Hello, null), document.getElementById("app"));
+export var BookshopRouter = function () {
+    return (React.createElement(Router, null,
+        React.createElement("div", null,
+            React.createElement(Route, { exact: true, path: "/", component: BookshopApp }),
+            React.createElement(Route, { path: "/book/:id", component: BookDetail }))));
+};
+ReactDOM.render(React.createElement(BookshopRouter, null), document.getElementById("app"));
 //# sourceMappingURL=index.js.map
--- Bookshop.WebApi/src/index.tsx
+++ Bookshop.WebApi/src/index.tsx
@@ -1,10 +1,13 @@
 ﻿import  * as React from "react";
 import * as ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-toggle-md navbar-inverse bg-inverse fixed-top">
-      <a className="navbar-brand" href="#">Bookshop</a>
+      <Link to="/" className="navbar-brand">
+        Bookshop
+      </Link>
     </nav>
   );
 };
@@ -21,45 +24,107 @@ const BookCard: React.StatelessComponent<BookCardProps> = ({cardTitle, cardText}
       <div className="card-block">
         <h4 className="card-title">{cardTitle}</h4>
         <p className="card-text">{cardText}</p>
-        <a href="#" className="btn btn-primary">Go to Book</a>
+          <Link to="/book/1" className="btn btn-primary">
+            Go to Book
+          </Link>
       </div>
     </div>
   );
 };
 
-export const BookshopApp = () => {
+interface BookImageProps {
+  imageUrl: string,
+}
+
+const BookImage: React.StatelessComponent<BookImageProps> = ({ imageUrl }) => {
   return (
     <div>
-      <Navbar />
-      <div className="container">
-        <div className="bookshop">
-          <h1>Bookshop</h1>
-          <div className="row">
-            <div className="col-md-4 mt-3">
-              <BookCard 
-                cardTitle="Test" 
-                cardText="Some quick example text to build on the card title and make up the bulk of the card's content." />
-            </div>
-            <div className="col-md-4 mt-3">
-              <BookCard
-                cardTitle="Test2"
-                cardText="Some quick example text to build on the card title and make up the bulk of the card's content." />
-            </div>
-            <div className="col-md-4 mt-3">
-              <BookCard
-                cardTitle="Test2"
-                cardText="Some quick example text to build on the card title and make up the bulk of the card's content." />
-            </div>
-            <div className="col-md-4 mt-3">
-              <BookCard
-                cardTitle="Test2"
-                cardText="Some quick example text to build on the card title and make up the bulk of the card's content." />
-            </div>
-          </div>
-        </div>
+      This is the book image section.
+    </div>
+  );
+};
+
+interface BookInformationProps {
+  id: string,
+  title: string,
+  description: string,
+  price: number,
+}
+
+const BookInformation: React.StatelessComponent<BookInformationProps> = ({ id, title, description }) => {
+  return (
+    <div>
+      This is the book detail section.
+    </div>
+  );
+};
+
+const BookDetail = () => {
+  const bookimage: BookImageProps = {
+    imageUrl: "Test Url",
+  };
+
+  const bookInfo: BookInformationProps = {
+    id: "some_guid",
+    title: "Test Title",
+    description: "Test Description",
+    price: 12
+  };
+
+  return (
+    <div className="row">
+      <div className="col-md-6">
+        <BookImage {...bookimage} />
+      </div>
+      <div className="col-md-6">
+        <BookInformation {...bookInfo} />
       </div>
     </div>
   );
 }
 
-ReactDOM.render(<BookshopApp />, document.getElementById("app"));
+export const BookshopHome = () => {
+  return (
+      <div className="bookshop">
+        <h1>Bookshop</h1>
+        <div className="row">
+          <div className="col-md-4 mt-3">
+            <BookCard 
+              cardTitle="Test" 
+              cardText="Some quick example text to build on the card title and make up the bulk of the card's content." />
+          </div>
+          <div className="col-md-4 mt-3">
+            <BookCard
+              cardTitle="Test2"
+              cardText="Some quick example text to build on the card title and make up the bulk of the card's content." />
+          </div>
+          <div className="col-md-4 mt-3">
+            <BookCard
+              cardTitle="Test2"
+              cardText="Some quick example text to build on the card title and make up the bulk of the card's content." />
+          </div>
+          <div className="col-md-4 mt-3">
+            <BookCard
+              cardTitle="Test2"
+              cardText="Some quick example text to build on the card title and make up the bulk of the card's content." />
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export const BookshopRouter = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <div className="container">
+          <Route exact path="/" component={BookshopHome} />
+          <Route path="/book/:id" component={BookDetail} />
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+ReactDOM.render(<BookshopRouter />, document.getElementById("app"));
